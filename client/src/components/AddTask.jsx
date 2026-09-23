ca9d833ee8d290c1afadb90d30495e76dd5eda7e
Luiz Henrique <luizhmc@gmail.com>
2026-09-23 01:17:10 +0000

    chore: altera texto do botao para Adicionar Tarefa

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Alterar tarefa
+        Adicionar Tarefa
       </button>
       
       <Modal
